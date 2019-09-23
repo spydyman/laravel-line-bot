7210d6ae1b501e67b192bd81f4481b8e459f23df
ibuki <ibuki@ibuki.local>
2019-09-24 00:04:40 +0900

    linebotcontrollerメソッド追加

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
 {
@@ -15,5 +18,13 @@ class LineBotController extends Controller
     {
         Log::debug($request->header());
         Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
     }
 }
