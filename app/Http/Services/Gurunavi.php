6ee194f71650336be0913233a73da5f14d06cbeb
ibuki <ibuki@ibuki.local>
2019-09-26 01:11:49 +0900

    controller追加

---
+++ app/Http/Services/Gurunavi.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class Gurunavi
+{
+    private const RESTAURANTS_SEARCH_API_URL = 'https://api.gnavi.co.jp/RestSearchAPI/v3/';
+
+    public function searchRestaurants($word)
+    {
+        $client = new Client();
+        $response = $client
+            ->get(self::RESTAURANTS_SEARCH_API_URL, [
+                'query' => [
+                    'keyid' => env('GURUNAVI_ACCESS_KEY'),
+                    'freeword' => str_replace(' ', ',', $word),
+                ],
+                'http_errors' => false,
+                // getメソッドについて
+                // 第一引数には、リクエスト先のURLを渡す。
+                // 第二引数には、オプションとなる情報を連想配列で渡す。
+                // その連想配列の中で、'query'をキーとする連想配列でリクエストパラメータを指定する事ができる。
+            ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
